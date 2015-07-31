401253b4b53784724cdc8c1c29b8277f0d7e9208
Daniel Ng <dvng13@gmail.com>
2015-07-31 10:25:34 -0700

    add #removeClass #addClass #attr

--- jquery_lite.js
+++ jquery_lite.js
@@ -31,14 +31,51 @@
   };
 
   // this method should be able to accept either a jQuery-lite wrapped collection, an HTML element, or a string.
+  // Description: Insert content, specified by the parameter, to the end of each element in the set of matched elements.
   DomNodeCollection.prototype.append = function(thing) {
     if (type instanceof DomNodeCollection) {
-
+      this.collection.concat(thing.collection);
     } else if (typeof thing === "string") {
-
+      this.collection[0].outerHTML += thing;
     } else {
-      
+      this.collection.push(thing);
     }
   };
+
+  DomNodeCollection.prototype.attr = function(attrName) {
+    var val;
+    this.collection.forEach(function(node) {
+      if (node.hasAttribute(attrName)) {
+        val = node.getAttribute(attrName);
+      }
+    });
+
+    return val;
+  };
+
+  DomNodeCollection.prototype.addClass = function(className) {
+    this.collection.forEach(function(node) {
+      node.className = node.className.replace(className, "");
+      if (node.className === "") {
+        node.className += className;
+      } else {
+        node.className += " " + className;
+      }
+    });
+
+    return this.collection;
+  };
+
+  DomNodeCollection.prototype.removeClass= function(className) {
+    this.collection.forEach(function(node) {
+      if (something) {
+        node.className = node.classNamereplace(className + " ", "");
+      } else {
+        node.className = node.className.replace(className, "");
+      }
+    });
+  };
+
+
 
 })();
