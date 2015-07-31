636254d33b9303a7b18b415fcfcdaf1625bf924f
Daniel Ng <dvng13@gmail.com>
2015-07-30 17:44:02 -0700

    add #empty and #append

--- jquery_lite.js
+++ jquery_lite.js
@@ -2,7 +2,7 @@
   window.$l = function(selector) {
     var elementList = [];
     if (typeof selector === "string") {
-      elementList.concat(Array.prototype.slice.call(document.querySelectorAll(selector)));
+      elementList = elementList.concat(Array.prototype.slice.call(document.querySelectorAll(selector)));
     } else if (selector instanceof HTMLElement) {
       console.log("HTML ELEMENT!");
       elementList.push(selector);
@@ -14,9 +14,31 @@
     this.collection = array;
   };
 
-  DomNodeCollection.prototype.html = function() {
-
+  DomNodeCollection.prototype.html = function(string) {
+    if (string) {
+      this.collection.forEach(function(node) {
+        node.innerHTML = string;
+      });
+    } else {
+      return this.collection[0].innerHTML;
+    }
   };
 
+  DomNodeCollection.prototype.empty = function() {
+    this.collection.forEach(function(node) {
+      node.innerHTML = "";
+    });
+  };
+
+  // this method should be able to accept either a jQuery-lite wrapped collection, an HTML element, or a string.
+  DomNodeCollection.prototype.append = function(thing) {
+    if (type instanceof DomNodeCollection) {
+
+    } else if (typeof thing === "string") {
+
+    } else {
+      
+    }
+  };
 
 })();
